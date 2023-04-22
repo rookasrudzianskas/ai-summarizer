12042bc901e1dd919d6aa77bd627bd4360f50014
Rokas <rookas.rudzenskas@gmail.com>
2023-04-22 10:47:55 +0300

    Add a GitHub button to the Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,15 @@ const Hero = () => {
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
         <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
+        <button
+          type='button'
+          onClick={() =>
+            window.open("https://github.com/rookasrudzianskas", "_blank")
+          }
+          className='black_btn'
+        >
+          GitHub
+        </button>
       </nav>
     </header>
   );
