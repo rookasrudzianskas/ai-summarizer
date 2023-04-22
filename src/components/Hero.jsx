2f53a5cc198b3a543e8a6fc0ca43efad7614f89e
Rokas <rookas.rudzenskas@gmail.com>
2023-04-22 10:50:25 +0300

    Add a title to the Hero component that promotes the use of OpenAI GPT-4 for article summarization

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,6 +17,11 @@ const Hero = () => {
           GitHub
         </button>
       </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden"/>
+        <span className='orange_gradient '>OpenAI GPT-4</span>
+      </h1>
     </header>
   );
 };
