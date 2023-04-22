e329dea22801664930bf1616778b6658b1e17366
Rokas <rookas.rudzenskas@gmail.com>
2023-04-22 11:04:34 +0300

    Remove the unused reducer in store.js

--- src/services/store.js
+++ src/services/store.js
@@ -4,7 +4,7 @@ import { articleApi } from "./article";
 
 export const store = configureStore({
     reducer: {
-        [articleApi.reducerPath]: articleApi.reducer,
+
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware)
 })
