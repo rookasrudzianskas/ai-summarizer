2bf710c37372fcb538ba6b4075337c2edb26d486
Rokas <rookas.rudzenskas@gmail.com>
2023-04-22 10:57:43 +0300

    Add placeholder and change class name in Demo.jsx input element

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
 
       <div className='app'>
         <Hero />
-        {/*<Demo />*/}
+        <Demo />
       </div>
     </main>
   );
--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -74,71 +74,20 @@ const Demo = () => {
           />
 
           <input
-            type='url'
-            placeholder='Paste the article link'
-            value={article.url}
-            onChange={(e) => setArticle({ ...article, url: e.target.value })}
-            onKeyDown={handleKeyDown}
+            value={""}
+            onChange={(e) => {}}
             required
-            className='url_input peer' // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
+            className="url_input peer"
+            placeholder="Enter a URL"
+            type="url"
           />
           <button
-            type='submit'
+            type="submit"
             className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
           >
             <p>↵</p>
           </button>
         </form>
-
-        {/* Browse History */}
-        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
-          {allArticles.reverse().map((item, index) => (
-            <div
-              key={`link-${index}`}
-              onClick={() => setArticle(item)}
-              className='link_card'
-            >
-              <div className='copy_btn' onClick={() => handleCopy(item.url)}>
-                <img
-                  src={copied === item.url ? tick : copy}
-                  alt={copied === item.url ? "tick_icon" : "copy_icon"}
-                  className='w-[40%] h-[40%] object-contain'
-                />
-              </div>
-              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
-                {item.url}
-              </p>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      {/* Display Result */}
-      <div className='my-10 max-w-full flex justify-center items-center'>
-        {isFetching ? (
-          <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
-        ) : error ? (
-          <p className='font-inter font-bold text-black text-center'>
-            Well, that wasn't supposed to happen...
-            <br />
-            <span className='font-satoshi font-normal text-gray-700'>
-              {error?.data?.error}
-            </span>
-          </p>
-        ) : (
-          article.summary && (
-            <div className='flex flex-col gap-3'>
-              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
-                Article <span className='blue_gradient'>Summary</span>
-              </h2>
-              <div className='summary_box'>
-                <p className='font-inter font-medium text-sm text-gray-700'>
-                  {article.summary}
-                </p>
-              </div>
-            </div>
-          )
-        )}
       </div>
     </section>
   );
